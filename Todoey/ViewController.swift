a496938d0ae924e6a0c878fdd1f17f3fc7090db4
Lukáš Všetečka <lukas.vsetecka@synetech.cz>
2019-06-11 14:54:15 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Lukáš Všetečka on 11/06/2019.
+//  Copyright © 2019 Lukáš Všetečka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
